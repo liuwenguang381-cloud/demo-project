b28e18dd43d358602a5abfee4e2d37b12562e8fb
liuwenguang381-cloud <liuwenguang381@gmail.com>
2026-04-17 14:39:07 +0800

    Refactor project structure and tooling

---
+++ src/utils/response.js
@@ -0,0 +1,14 @@
+function sendJson(res, statusCode, data) {
+  res.writeHead(statusCode, { 'Content-Type': 'application/json; charset=utf-8' });
+  res.end(JSON.stringify(data, null, 2));
+}
+
+function sendText(res, statusCode, text) {
+  res.writeHead(statusCode, { 'Content-Type': 'text/plain; charset=utf-8' });
+  res.end(text);
+}
+
+module.exports = {
+  sendJson,
+  sendText,
+};
