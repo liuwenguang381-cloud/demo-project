273a00e63a40236dfad08bff68dc8f20e65880ea
liuwenguang381-cloud <liuwenguang381@gmail.com>
2026-04-17 14:42:38 +0800

    Refactor project structure and tooling

---
+++ src/app.js
@@ -0,0 +1,71 @@
+const { URL } = require('url');
+const { sendJson } = require('./utils/response');
+const { handleRoot } = require('./routes/root');
+const { handleHealth } = require('./routes/health');
+const { handleInfo } = require('./routes/info');
+const { handleEcho } = require('./routes/echo');
+
+function methodNotAllowed(res, allowedMethods) {
+  sendJson(res, 405, {
+    error: 'Method Not Allowed',
+    allowedMethods,
+  });
+}
+
+function notFound(res, pathname, method) {
+  sendJson(res, 404, {
+    error: 'Not Found',
+    path: pathname,
+    method,
+  });
+}
+
+function createApp() {
+  return function app(req, res) {
+    const url = new URL(req.url, `http://${req.headers.host || 'localhost'}`);
+    const { pathname } = url;
+    const method = req.method || 'GET';
+
+    if (pathname === '/') {
+      if (method !== 'GET') {
+        methodNotAllowed(res, ['GET']);
+        return;
+      }
+      handleRoot(req, res);
+      return;
+    }
+
+    if (pathname === '/api/health') {
+      if (method !== 'GET') {
+        methodNotAllowed(res, ['GET']);
+        return;
+      }
+      handleHealth(req, res);
+      return;
+    }
+
+    if (pathname === '/api/info') {
+      if (method !== 'GET') {
+        methodNotAllowed(res, ['GET']);
+        return;
+      }
+      handleInfo(req, res);
+      return;
+    }
+
+    if (pathname === '/api/echo') {
+      if (method !== 'GET') {
+        methodNotAllowed(res, ['GET']);
+        return;
+      }
+      handleEcho(req, res, url);
+      return;
+    }
+
+    notFound(res, pathname, method);
+  };
+}
+
+module.exports = {
+  createApp,
+};
